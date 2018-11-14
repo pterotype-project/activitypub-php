778bdfe6b8790408f91c44440471aee981daf31d
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-11-14 08:08:15 -0500

    Set up tests and change directory structure

---
+++ test/ActivityPubTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ActivityPubTest extends TestCase {
+    public function testItWillPass() {
+        // TODO fix me
+        $this->assertSame( 1, 0 );
+    }
+}
+?>
