b78e648c62bb7623f6c26bf05f104ed2cd5393f3
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-11-20 07:11:20 -0500

    Flesh out index and object entities

--- src/Entities/IndexEntity.php
+++ src/Entities/IndexEntity.php
@@ -17,10 +17,106 @@ namespace ActivityPub\Entities;
  */
 class IndexEntity
 {
-    // TODO what should the primary key be?
+    /**
+     * @ManyToOne(targetEntity="ObjectEntity", inversedBy="subjectIndices")
+     * @var ObjectEntity The subject of the index row
+     */
     protected $subject;
+    /**
+     * @Column(type="string")
+     * @var string The predicate of the index row
+     */
     protected $predicate;
+    /**
+     * @Column(type="string")
+     * @var string The value of the index row; mutually exclusive with $object
+     */
     protected $value;
+    /**
+     * @ManyToOne(targetEntity="ObjectEntity", inversedBy="objectIndices")
+     * @var ObjectEntity The object of the index row; mutually exclusive with $value
+     */
     protected $object;
+
+    /**
+     * Create a new index row with a string value
+     *
+     * @param ObjectEntity $subject The subject of the index row
+     * @param string $predicate The predicate of the index row
+     * @param string $value The value of the index row
+     * @return IndexEntity The new index row
+     */
+    public static function withValue(ObjectEntity $subject, string $predicate, string $value) {
+        $idx = new IndexEntity();
+        $idx->setSubject( $subject );
+        $idx->setPredicate( $predicate );
+        $idx->setValue( $value );
+        return $idx;
+    }
+
+    /**
+     * Create a new index row that references another object
+     *
+     * @param ObjectEntity $subject The subject of the index row
+     * @param string $predicate The predicate of the index row
+     * @param ObjectEntity $object The object of the index row
+     * @return IndexEntity The new index row
+     */
+    public static function withObject(ObjectEntity $subject, string $predicate, ObjectEntity $object) {
+        $idx = new IndexEntity();
+        $idx->setSubject( $subject );
+        $idx->setPredicate( $predicate );
+        $idx->setObject( $object );
+        return $idx;
+    }
+
+    protected function setSubject(ObjectEntity $subject) {
+        $subject->addSubjectIndex( $this );
+        $this->subject = $subject;
+    }
+
+    protected function setObject(ObjectEntity $object) {
+        $object->addObjectIndex( $this );
+        $this->object = $object;
+    }
+
+    protected function setPredicate(string $predicate) {
+        $this->predicate = $predicate;
+    }
+
+    protected function setValue(string $value) {
+        $this->value = $value;
+    }
+
+    /**
+     * Returns the subject of the index row
+     *
+     * @return ObjectEntity
+     */
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    /**
+     * Returns the predicate of the index row
+     *
+     * @return string
+     */
+    public function getPredicate() {
+        return $this->predicate;
+    }
+
+    /**
+     * Returns either the value or the object of the index row, depending on which was set
+     *
+     * @return string|ObjectEntity
+     */
+    public function getValueOrObject() {
+        if ( ! is_null( $this->object ) ) {
+            return $this->object;
+        } else {
+            return $this->value;
+        }
+    }
 }
 ?>
--- src/Entities/ObjectEntity.php
+++ src/Entities/ObjectEntity.php
@@ -1,6 +1,8 @@
 <?php
 namespace ActivityPub\Entities;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * Represents an ActivityPub JSON-LD object
  * @Entity @Table(name="objects")
@@ -15,9 +17,36 @@ class ObjectEntity
      */
     protected $id;
 
+    /**
+     * Indices for which this object is the subject
+     * @OneToMany(targetEntity="IndexEntity", mappedBy="subject")
+     * @var IndexEntity[] An ArrayCollection of IndexEntities
+     */
+    protected $subjectIndices;
+
+    /**
+     * Indices for which this object is the object
+     * @OneToMany(targetEntity="IndexEntity", mappedBy="object")
+     * @var IndexEntity[] An ArrayCollection of IndexEntities
+     */
+    protected $objectIndices;
+
+    public function __construct() {
+        $this->subjectIndex = new ArrayCollection();
+        $this->objectIndex = new ArrayCollection();
+    }
+
     public function getId()
     {
         return $this->id;
+    }
+
+    public function addSubjectIndex(IndexEntity $idx) {
+        $this->subjectIndices[] = $idx;
+    }
+
+    public function addObjectIndex(IndexEntity $idx) {
+        $this->objectIndices[] = $idx;
     }
 }
 ?>
