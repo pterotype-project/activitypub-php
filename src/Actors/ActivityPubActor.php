c74f81cac333a4e704f32a572bafafbdf40bfe18
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-12-31 12:39:38 -0500

    [WIP] Outline the Inbox, Outbox, and Actor classes

--- src/Actors/ActivityPubActor.php
+++ src/Actors/ActivityPubActor.php
@@ -1,14 +1,93 @@
 <?php
 namespace ActivityPub\Actors;
 
+use ActivityPub\Crypto\RsaKeypair;
+use ActivityPub\Entities\ActivityPubObject;
+
 /**
  * Represents an ActivityPub actor object
  *
  * This class is the main entrypoint for the ActivityPub API, via the
- *   post_to_inbox() and post_to_outbox() methods.
+ * inbox() and outbox() methods.
  */
 class ActivityPubActor
 {
-    // TODO
+    /**
+     * The ActivityPubObject that represents this actor
+     *
+     * @var ActivityPubObject
+     */
+    private $object;
+
+    /**
+     * The actor's inbox
+     *
+     * @var Inbox
+     */
+    private $inbox;
+
+    /**
+     * The actor's outbox
+     *
+     * @var Outbox
+     */
+    private $outbox;
+
+    /**
+     * The actor's RSA keypair
+     *
+     * @var RsaKeypair
+     */
+    private $keypair;
+
+    /**
+     * Constructs a new actor
+     *
+     * @param ActivityPubObject $object The ActivityPubObject that represents the actor
+     * @param RsaKeypair $keypair The actor's RSA keypair
+     */
+    public function __construct( ActivityPubObject $object,
+                                    RsaKeypair $keypair )
+    {
+        if ( ! $object['inbox'] || ! $object['outbox'] ) {
+            throw new InvalidArgumentException(
+                'Attempted to construct an ActivityPubActor without an inbox or outbox'
+            );
+        }
+        $this->object = $object;
+        $this->keypair = $keypair;
+        $this->inbox = new Inbox( $this, $object['inbox'] );
+        $this->outbox = new Outbox( $this, $object['outbox'] );
+    }
+
+    /**
+     * Returns the actor's inbox
+     *
+     * @return Inbox
+     */
+    public function inbox()
+    {
+        return $this->inbox;
+    }
+
+    /**
+     * Returns the actor's outbox
+     *
+     * @return Outbox
+     */
+    public function outbox()
+    {
+        return $this->outbox;
+    }
+
+    /**
+     * Returns the actor's keypair
+     *
+     * @return RsaKeypair
+     */
+    public function keypair()
+    {
+        return $this->keypair;
+    }
 }
 ?>
---
+++ src/Actors/Outbox.php
@@ -0,0 +1,51 @@
+<?php
+namespace ActivityPub\Actors;
+
+use ActivityPub\Actors\ActivityPubActor;
+use ActivityPub\Entities\ActivityPubObject;
+
+/**
+ * The Outbox class represents an ActivityPub outbox. It provides methods
+ * to post activities to an actor's outbox, and takes care of signing and
+ * delivering posted activities.
+ */
+class Outbox
+{
+    /**
+     * The ActivityPubObject that represents this outbox
+     *
+     * @var ActivityPubObject
+     */
+    private $object;
+
+    /**
+     * The actor to which this outbox belongs
+     *
+     * @var ActivityPubActor
+     */
+    private $actor;
+
+    /**
+     * Constructs a new Outbox
+     *
+     * @param ActivityPubActor $actor The actor to which the outbox belongs
+     * @param ActivityPubObject $object The ActivityPubObject that represents the outbox 
+     */
+    public function __construct( ActivityPubActor $actor, ActivityPubObject $object )
+    {
+        $this->actor = $actor;
+        $this->object = $object;
+    }
+
+    /**
+     * Posts a new activity to the outbox, signing and delivering the activity
+     * to the correct recipients.
+     *
+     * @param ActivityPubObject $activity The activity
+     */
+    public function post( ActivityPubObject $activity )
+    {
+        // TODO
+    }
+}
+?>
